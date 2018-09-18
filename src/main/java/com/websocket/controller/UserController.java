8802f76a78fcc1c4e9cd0d77317da6118c3144d6
jagasan dansena <jagasan.dansena@techo2>
2018-09-18 18:32:16 +0530

    changed que

--- src/main/java/com/websocket/controller/UserController.java
+++ src/main/java/com/websocket/controller/UserController.java
@@ -7,8 +7,9 @@ package com.websocket.controller;
 
 import com.websocket.model.User;
 import com.websocket.model.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -18,11 +19,15 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class UserController {
     
-    
+          @Autowired
+    SimpMessagingTemplate template;
     @MessageMapping("/user") // getting data from client side(browser)
-    @SendTo("/topic/user")
-    public UserResponse getUser(User user){
-        return new UserResponse("hi ... "+user.getName());
+   // @SendTo("/topic/user")
+    public void getUser(User user){
+      //  return new UserResponse("hi ... "+user.getName());
+           template.convertAndSend("/topic/user", new UserResponse("hi ... "+user.getName()));
+          //template.
+           
     }
     
 }
